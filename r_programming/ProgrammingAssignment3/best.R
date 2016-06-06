127c7427ae71a3c53c3ba51e48746738ea77d695
Delaney, Alan <hairy.one@virgin.net>
2016-06-06 03:22:56 +0100

    First of three functions for final exam

---
+++ r_programming/ProgrammingAssignment3/best.R
@@ -0,0 +1,44 @@
+best <- function(state, condition) {
+  
+  if (condition == "heart attack") {
+    
+    condition = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+    
+  } else if (condition == "heart failure") {
+    
+    condition = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+    
+  } else if (condition == "pneumonia") {
+    
+    condition = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+    
+  } else {
+    
+    stop ("invalid outcome")
+    
+  }
+
+  if ((class (state) != "character") | (length (state) != 1)) {
+    
+    return ("invalid state 1")
+    
+  }
+  
+  #print (outcome$State)
+  
+  sList <- outcome [outcome$State == state,]
+  #print (length (sList))
+  
+  if (nrow (sList) == 0) {
+    
+    return ("invalid state 2")
+    
+  }
+  
+  tMin <- min (as.numeric (sList [[condition]]), na.rm = TRUE)
+  sList <- sList [as.numeric (sList [[condition]]) == tMin, ]
+  sList <- sort (sList$Hospital.Name)
+  sList [1]
+
+}
+
